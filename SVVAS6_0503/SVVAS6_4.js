2964825007fc8343ea5889087fa4ebc8f39df3ab
Sznikolet <90954448+Sznikolet@users.noreply.github.com>
2024-05-07 21:36:19 +0200

    Add files via upload

---
+++ SVVAS6_0503/SVVAS6_4.js
@@ -0,0 +1,35 @@
+function hide3() {
+    $("p").hide();
+  }
+  
+  function show3() {
+    $("p").show();
+  }
+  
+  function showHide() {
+    $("p").toggle();
+  }
+  
+  function opacity1() {
+    $("#form").css("opacity", 0.1);
+  }
+  
+  function opacity2() {
+    $("#form").css("opacity", 0.5);
+  }
+  
+  function opacity3() {
+    $("#form").css("opacity", 0.8);
+  }
+  
+  function hideForm() {
+    $("#form").hide();
+  }
+  
+  function showForm() {
+    $("#form").show();
+  }
+  
+  function hideShowForm() {
+    $("#form").toggle();
+  }
